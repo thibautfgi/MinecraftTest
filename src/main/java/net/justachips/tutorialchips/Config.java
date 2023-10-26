b0c475877dcc4b19566a9b6d899e019e74194468
thibaut <thibaut.fgi@hotmail.com>
2023-10-26 12:09:49 +0200

    setup v2

--- src/main/java/net/justachips/tutorialchips/Config.java
+++ src/main/java/net/justachips/tutorialchips/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.justachips.tutorialchips;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -13,9 +13,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import net.justachips.tutorialchips.TutorialChips;
+
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
-@Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = TutorialChips.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
